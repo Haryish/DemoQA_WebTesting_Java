924d1252c44c6fe1d0b7e8ec53212d73d0a3fdc5
Haryish E <haryishkumaran16@gmail.com>
2025-11-18 12:50:02 +0530

    Proper E2E Run Initiated

--- src/test/java/com/demoqa/test/MOD01_Elements.java
+++ src/test/java/com/demoqa/test/MOD01_Elements.java
@@ -16,40 +16,33 @@ public class MOD01_Elements extends BaseTest {
     public String pageName;
 
     @Test(groups = {"smoke"})
-    public void verifyPageTitle() {
+    public void PRECHECK_01_verifyPageTitle() {
         Assert.assertEquals(driver.getTitle(), "DEMOQA");
     }
 
     @Test(groups = {"healthCheck"})
-    public void verifyPageUrl() {
+    public void PRECHECK_02_verifyPageUrl() {
         Assert.assertTrue(driver.getCurrentUrl().contains("demoqa.com"));
     }
 
-    @Test
-    public void verifyLogoIsDisplayed() {
+    @Test(enabled = false)
+    public void PRECHECK_03_verifyLogoIsDisplayed() {
         Assert.assertTrue(driver.findElement(By.id("app")).isDisplayed());
     }
 
-    @Test(groups = {"E2E", "Regression", "Elements"})
+    @Test(groups = {"E2E", "Regression", "Elements"},enabled = false)
     public void MOD_01_TS_001_ValidateTextInputSubmission() throws InterruptedException {
         /*
-        * 1. Navigate to Text Box page
-        * 2. Enter "John Doe" in Full Name field
-        * 3. Enter "test@example.com" in Email field
-        * 4. Click Submit
-        * 5. Verify output displays entered values
-        * */
+         * 1. Navigate to Text Box page
+         * 2. Enter "John Doe" in Full Name field
+         * 3. Enter "test@example.com" in Email field
+         * 4. Click Submit
+         * 5. Verify output displays entered values
+         * */
         pageName = "Elements";
 
-        driver.findElement(By.xpath(String.format("//div[@class='card-body']/h5[text()='%s']",pageName))).click();
+        driver.findElement(By.xpath(String.format("//div[@class='card-body']/h5[text()='%s']", pageName))).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[starts-with(text(),'Please')]")));
-
-
-
-
-
-
     }
-
 }
